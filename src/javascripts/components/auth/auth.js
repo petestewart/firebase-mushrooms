44fb893010caa6c809b3ed7b90cd0cd7b9712bb4
Pete Stewart <petestew@gmail.com>
2020-06-20 11:57:02 -0500

    login and logout work

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,7 +1,10 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import utils from '../../helpers/utils';
 
 const signMeIn = () => {
-  console.error('signing you in');
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 };
 
 const loginButton = () => {
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,18 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/myNavbar/myNavbar';
 import 'bootstrap';
 import '../styles/main.scss';
 
 console.error('test');
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
